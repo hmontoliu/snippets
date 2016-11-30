8ec33aab46dc28c1ce09454dc3978b9c7af1b595
Hilario J. Montolliu <hmontoliu@gmail.com>
2016-11-30 17:18:00 +0100

    Quitar los iconos de W3C por innecesarios

--- informemigracion/informemigracionFormato.ps1
+++ informemigracion/informemigracionFormato.ps1
@@ -104,11 +104,6 @@ $footer = '
 </div>
 <div id="pie">
 <p>
-    <a href="http://validator.w3.org/check?uri=referer">
-    <img style="border:0;width:88px;height:31px" src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Transitional"/></a>
-    <a href="http://jigsaw.w3.org/css-validator/check/referer">
-        <img style="border:0;width:88px;height:31px" src="http://jigsaw.w3.org/css-validator/images/vcss" alt="Valid CSS!" />
-    </a>
 </p>
 <div id="autor">
 Hilario J. Montoliu (hmontoliu at gmail.com)
