a4645525661d44893cc73cec533f3514a07b4720
Hilario J. Montolliu <hmontoliu@gmail.com>
2017-01-18 11:28:07 +0100

    ccleanermant - testing

--- mantmisc/ccleanermant.ps1
+++ mantmisc/ccleanermant.ps1
@@ -1,5 +1,11 @@
-# Descarga e instalación de ccleaner (test)
+# Download, install, and run free ccleaner
+# You should run ccleaner if you have configured it previously 
+#
+# -- H. Montoliu <hmontoliu@gmail.com>  Wed Jan 18 10:27:01 UTC 2017
+#
 $VERSION="526"
+
+# download
 $URL="http://download.piriform.com/ccsetup${VERSION}.exe"
 $DESTDIR="c:\_administrador\programas"
 $INSTALLER="ccsetup${VERSION}.exe"
@@ -7,9 +13,10 @@ mkdir -force $DESTDIR
 # (New-Object System.Net.WebClient).DownloadFile($URL, $DESTDIR + "\" + [System.IO.Path]::GetFileName($URL))
 (New-Object System.Net.WebClient).DownloadFile($URL, $DESTDIR + "\" + $INSTALLER)
 
-# instalacion
-cd $DESTDIR
-&$INSTALLER "/S"
+# install
+&$DESTDIR\$INSTALLER "/S"
 
-
+# run
+$ccleaner = get-childitem "C:\prog*\ccleaner" -include ccleaner.exe -recurse
+&$ccleaner "/auto"
 
