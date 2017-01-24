a74a6f7383e002e267d30bfc427238480aa449ad
Hilario J. Montolliu <hmontoliu@gmail.com>
2017-01-24 12:56:52 +0100

    dos2unix conversion, add many TODO sutff, minor updates, still draft

--- mantmisc/mantenimiento.ps1
+++ mantmisc/mantenimiento.ps1
@@ -2,9 +2,12 @@
 # draft
 # -- H. Montoliu <hmontoliu@gmail.com>  Wed Jan 18 10:27:01 UTC 2017
 
+# TODO
+# use system env vars
+# integrate legacy systems
+
 
 # ERROR/SYS LOG
-
 get-eventlog -logname system -entrytype error,warning -newest 50 | Select-Object Index,TimeGenerated,EntryType,Source,EventID,InstanceId,Message | Out-GridView
 get-eventlog -logname application -entrytype error,warning -newest 50 | Select-Object Index,TimeGenerated,EntryType,Source,EventID,InstanceId,Message | Out-GridView
 
@@ -16,19 +19,17 @@ gci 'C:\program files*\Cobian*\Logs\*' | sort LastWriteTime | select -last 7 | s
 # legacy windows TODO
 gci 'C:\Arch*\Cobian*\Settings\Logs\*' | sort LastWriteTime | select -last 7 | select-string 'error' -Context 4 | out-gridview
 
-# PROGRAM DOWNLOAD AND INSTALL
-$DESTDIR="c:\_administrador\programas"
-mkdir -force $DESTDIR
-
 # ROBOCOPY BACKUP SUMMARY
 # TODO
 # gci c:\backups\logs\* | select-string -Pattern 'Started|error|Copied|Inicio|error|Copiado' -Context 4
 
+# PROGRAM DOWNLOAD AND INSTALL
+$DESTDIR="c:\_administrador\programas"
+mkdir -force $DESTDIR
+
 # CCLEANER
 # Download, install, and run free ccleaner
 # You should run ccleaner if you have configured it previously 
-#
-#
 $VERSIONCC="526"
 
 # download
@@ -43,8 +44,11 @@ $INSTALLER="ccsetup${VERSIONCC}.exe"
 
 # run ccleaner
 $ccleaner = get-childitem "C:\prog*\ccleaner" -include ccleaner.exe -recurse
+# legacy TODO
 # do not run ccleaner until eventlog is fully checked
 # &$ccleaner "/auto"
+# launch ccleaner in interactive mode
+&$ccleaner
 
 # DEFRAGGLER
 $VERSIONDF="221"
@@ -57,14 +61,21 @@ $INSTALLER="dfsetup${VERSIONDF}.exe"
 # install
 &$DESTDIR\$INSTALLER "/S"
 
-# Malwarebytes
+# run defraggler
+$defraggler = get-childitem "C:\prog*\defraggler" -include defraggler.exe -recurse
+# legacy TODO
+# launch defraggler in interactive mode
+&$defraggler
+
+# MALWAREBYTES
 # TODO Download and install
 # launch mbam
 $mbam = get-childitem "C:\prog*\malwarebytes*" -include mbam.exe -recurse
 # legacy windows launch mbam TODO
-$mbam = get-childitem "C:\arch*\malwarebytes*" -include mbam.exe -recurse
+# $mbam = get-childitem "C:\arch*\malwarebytes*" -include mbam.exe -recurse
 &$mbam
 
+# COMPMGMT
 # open compmgmt.msc at end
 $compmgmt="compmgmt.msc"
 &$compmgmt
