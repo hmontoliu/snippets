039ea6516bfb980e40e00d476bfd314d9d36400b
Marcos Leal Sierra <marcoslealsierra90@gmail.com>
2017-02-13 10:59:10 +0100

    Mejoras imprescindibles

--- mantmisc/ninite.ps1
+++ mantmisc/ninite.ps1
@@ -1,10 +1,13 @@
+# -- Marcos Leal Sierra <marcoslealsierra90@gmail.com> lunes 13 de febrero del 2017 a las 10:54:09
+
 Write-Host Ninite autoinstall
 
-# Get item names by reading ninite.exe download url
+# Programas a descargar
 $items = @("winrar", "chrome", "java", "notepadplusplus", "thunderbird", 
 "vlc", "pdfcreator", "essentials", "malwarebytes")
 
-# Create URL
+# Generar URL
+$url = ""
 foreach ($item in $items)
 { 
   $url += $item + "-"
@@ -12,7 +15,7 @@ foreach ($item in $items)
 
 $url = "http://www.ninite.com/" + $url.TrimEnd("-") + "/ninite.exe"
 
-# Download 
+# Descarga y ejecución 
 $file = [system.environment]::getenvironmentvariable("userprofile") + "\Downloads\ninite.exe"
 $webclient = New-Object System.Net.WebClient
 $webclient.DownloadFile($url,$file)
