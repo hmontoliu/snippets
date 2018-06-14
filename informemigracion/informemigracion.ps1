b8ede0018677cd16328e0cde73032978fde51c9d
Fer <fbrines@ebmproyectos.com>
2018-06-14 12:50:02 +0200

    Añadir propiedades a las impresoras

--- informemigracion/informemigracion.ps1
+++ informemigracion/informemigracion.ps1
@@ -229,7 +229,7 @@ $logicaldisk = Get-WMIObject Win32_logicaldisk |
      Out-String
  
 $impresoras = Get-WMIObject Win32_printer |
-     ConvertTo-Html -Fragment name, sharename, status `
+     ConvertTo-Html -Fragment name, sharename, status, portname, drivername `
      -precontent "<div id='printers'><h3>Impresoras</h3>"`
      -postcontent "</div>" | 
      Out-String
